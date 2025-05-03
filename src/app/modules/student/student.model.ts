8549db44fc07224b06d71241961242038352fc43
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2025-05-04 00:03:59 +0600

    custom validation

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -7,25 +7,51 @@ import {
 } from './student.interface';
 
 const userNameSchema = new Schema<Username>({
-  firstName: { type: String, require: [true, 'First name is required'] }, //mongoose এ required field validation message pass করা যায়। যদি field না পাঠানো হয় তাহলে error message show করবে ।
-  middleName: { type: String },
-  lastName: { type: String, require: [true, 'last name is required '] },
+  firstName: {
+    type: String,
+    require: [true, 'First name is required'], //mongoose এ required field validation message pass করা যায়। যদি field না পাঠানো হয় তাহলে error message show করবে ।
+    maxlength: [15, 'first name max length is 15'], //max character length validation
+    trim: true, // text এর start-end space গুলো trim করার জন্য trim use করা হয়।
+    validate: {
+      validator: function (value: string) {
+        //Title case validation for firstname through custom validation
+        // validator function এর মধ্যে main value টা পাওয়া যায় যেটা client থেকে আসবে।
+        const firstName = value
+          .charAt(0)
+          .toUpperCase()
+          .concat(value.slice(1).toLowerCase());
+        return value === firstName; //return true or false
+      },
+      message: '{VALUE} is not Title Case',
+    },
+  },
+  middleName: {
+    type: String,
+    trim: true, // text এর start-end space গুলো trim করার জন্য trim use করা হয়।
+    maxlength: [15, 'middle name max length is 15'],
+  },
+  lastName: {
+    type: String,
+    require: [true, 'last name is required '],
+    trim: true,
+    maxlength: [15, 'last name max length is 15'],
+  },
 });
 
 const guardianSchema = new Schema<Guardian>({
-  fatherName: { type: String },
-  fatherContactNo: { type: String },
-  fatherOccupation: { type: String },
-  motherName: { type: String },
-  motherContactNo: { type: String },
-  motherOccupation: { type: String },
+  fatherName: { type: String, trim: true },
+  fatherContactNo: { type: String, trim: true },
+  fatherOccupation: { type: String, trim: true },
+  motherName: { type: String, trim: true },
+  motherContactNo: { type: String, trim: true },
+  motherOccupation: { type: String, trim: true },
 });
 
 const localGuardianSchema = new Schema<LocalGuardian>({
-  name: { type: String },
-  contactNo: { type: String },
-  occupation: { type: String },
-  address: { type: String },
+  name: { type: String, trim: true },
+  contactNo: { type: String, trim: true },
+  occupation: { type: String, trim: true },
+  address: { type: String, trim: true },
 });
 
 const studentSchema = new Schema<Student>({
@@ -34,34 +60,42 @@ const studentSchema = new Schema<Student>({
     type: userNameSchema,
     required: [true, 'Name is required'], //mongoose এ required field validation message pass করা যায়। যদি field না পাঠানো হয় তাহলে error message show করবে ।
   },
-  email: { type: String, required: true, unique: true }, // unique হলো এই field value unique হতে হবে, duplicate হবে পারবে না। field unique করলে auto indexing হয়ে যায়।
+  email: { type: String, required: true, unique: true, trim: true }, // unique হলো এই field value unique হতে হবে, duplicate হবে পারবে না। field unique করলে auto indexing হয়ে যায়।
   gender: {
     type: String,
     enum: {
-      values: ['male', 'female'], // enum value গুলো values property এর মধ্যে দিতে হবে।
+      values: ['male', 'female'], // enum value গুলো values property এর মধ্যে array আকারে দিতে হবে।
       message: '{VALUE} is not valid', //enum এর ক্ষেত্রে message property তে error message পাঠাতে হবে। {VALUE} এর মধ্যে user যে value পাঠাবে সেটা পাওয়া যাবে।
     }, // এখানে ['male', 'female'] কে mongoose ename type বলে। male অথবা female যেকোনো একটা বসবে, union type এর মতো।
     required: true,
   },
   dateOfBirth: { type: String },
-  contactNo: { type: String, required: [true, 'contactNo is required'] },
+  contactNo: { type: String, required: [true, 'contact no is required'] },
   emergencyContactNo: { type: String },
   bloodGroup: {
     type: String,
-    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'], //enum যেকোনো একটা নিবে এটা mongoose built in property
+    enum: {
+      values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'], //enum যেকোনো একটা নিবে এটা mongoose built in property
+      message: '{VALUE} is not valid',
+    },
   },
   isActive: {
     type: String,
-    enum: ['active', 'blocked'],
+    enum: {
+      values: ['active', 'blocked'],
+      message: '{VALUE} is not supported',
+    },
     default: 'active', // default হিসাবে active থাকবে
   },
   presentAddress: {
     type: String,
     required: [true, 'Present address is required'],
+    trim: true,
   },
   permanentAddress: {
     type: String,
     required: [true, 'Permanent address is required'],
+    trim: true,
   },
   guardian: {
     type: guardianSchema,
